312f6a0fcd8d545fb40bba945456d3ababec5a6b
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-02-14 19:03:21 +0000

    dia 14 02

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,8 @@
-// função para trocar a cor do texto dependendo do evento , que no caso e a passada do mouse 
-function TrocaElemento(element) {
- 
-    if (element.classList.contains("bg-opacity-100")) {
-      element.classList.remove("bg-opacity-100");
-      element.classList.add("bg-secondary");
-    } else {
-      element.classList.remove("bg-secondary");
-      element.classList.add("bg-opacity-100");
-    }
-  }
-  
-document.querySelector(".rounded-5").onmouseover = function() {
-  TrocaElemento(this);
-};
 
-document.querySelector(".rounded-5").onmouseout = function() {
-  TrocaElemento(this);
-};
+document.querySelector("#btnOpenModalSignin").addEventListener("click", function() {
+  document.querySelector("#modalSignin").style.display = "block";
+});
 
-
-
+document.querySelector(".btn-close").addEventListener("click", function() {
+  document.querySelector("#modalSignin").style.display = "none";
+});
