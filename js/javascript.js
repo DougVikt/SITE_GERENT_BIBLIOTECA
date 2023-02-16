c6728cb1891dde21b0509bdd3aace2b72ee4a803
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-02-16 17:52:34 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -46,3 +46,29 @@ meuLink2.addEventListener('click', function(event) {
     window.location.href = 'arquivo1.html';
   }
 });
+
+// obtém o elemento da página que irá mostrar o nome do usuário logado
+const usuarioLogado = document.getElementById('usuarioLogado');
+
+// faz uma requisição AJAX para o servidor para obter o nome do usuário logado
+const request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    // exibe o nome do usuário logado na página
+    usuarioLogado.innerHTML = 'Bem-vindo, ' + this.responseText;
+  }
+}
+request.open('GET', 'obter_nome_usuario.php');
+request.send();
+
+
+// JavaScript
+// verifique se o usuário está logado e é um funcionário
+let usuarioEstaLogado = true; // exemplo de variável para usuário logado
+let usuarioEFuncionario = true; // exemplo de variável para usuário funcionário
+
+// verifique se o usuário está logado e é um funcionário
+if (usuarioEstaLogado && usuarioEFuncionario) {
+  // mostra o link para a página do funcionário
+  document.getElementById("link-funcionario").style.display = "block";
+}
