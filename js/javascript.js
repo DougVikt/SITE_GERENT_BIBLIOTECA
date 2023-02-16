88a49a11bd424dde0e7f401940c95d443924507d
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-02-15 23:52:43 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,48 @@
 
-document.querySelector("#btnOpenModalSignin").addEventListener("click", function() {
-  document.querySelector("#modalSignin").style.display = "block";
+fetch('verifica_login.php')//nome arquivo de verificação 
+  .then(response => response.json())
+  .then(logado => {
+    if (logado) {
+      // Esconder botões de login e cadastro
+      document.getElementById('login').style.display = 'none';
+      document.getElementById('cadastro').style.display = 'none';
+    } else {
+      // Mostrar botões de login e cadastro
+      document.getElementById('login').style.display = 'block';
+      document.getElementById('cadastro').style.display = 'block';
+    }
+  })
+  .catch(error => console.error(error));
+
+
+const meuLink = document.getElementById('acervo');
+
+meuLink.addEventListener('click', function(event) {
+  // cancela o comportamento padrão da tag a
+  event.preventDefault();
+  
+  // verifica se o usuário está logado
+  if (logado) {
+    // redireciona o usuário para o arquivo2.html
+    window.location.href = 'arquivo2.html';
+  } else {
+    // redireciona o usuário para o arquivo1.html
+    window.location.href = 'arquivo1.html';
+  }
 });
 
-document.querySelector(".btn-close").addEventListener("click", function() {
-  document.querySelector("#modalSignin").style.display = "none";
+const meuLink2 = document.getElementById('praso');
+
+meuLink2.addEventListener('click', function(event) {
+  // cancela o comportamento padrão da tag a
+  event.preventDefault();
+  
+  // verifica se o usuário está logado
+  if (logado) {
+    // redireciona o usuário para o arquivo2.html
+    window.location.href = 'arquivo2.html';
+  } else {
+    // redireciona o usuário para o arquivo1.html
+    window.location.href = 'arquivo1.html';
+  }
 });
