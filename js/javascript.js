23b316c662986116e563973f11c6b082f357156b
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-02-23 17:50:07 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,26 @@
+function verificarLogado() {
+  fetch('login.php') // arquivo PHP que verifica se o usuário está logado
+    .then(response => response.json())
+    .then(logado => {
+      if (logado) {
+        // usuário está logado, então esconder botões de login e cadastro
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('cadastro').style.display = 'none';
 
-fetch('verifica_login.php')//nome arquivo de verificação 
-  .then(response => response.json())
-  .then(logado => {
-    if (logado) {
-      // Esconder botões de login e cadastro
-      document.getElementById('login').style.display = 'none';
-      document.getElementById('cadastro').style.display = 'none';
-    } else {
-      // Mostrar botões de login e cadastro
-      document.getElementById('login').style.display = 'block';
-      document.getElementById('cadastro').style.display = 'block';
-    }
-  })
-  .catch(error => console.error(error));
+        // mostrar nome do usuário logado
+        document.getElementById('usuario-logado').innerHTML = nomeUsuario;
+      } else {
+        // usuário não está logado, então mostrar botões de login e cadastro
+        document.getElementById('login').style.display = 'block';
+        document.getElementById('cadastro').style.display = 'block';
 
+        // esconder nome do usuário logado
+        document.getElementById('usuario-logado').innerHTML = '';
+      }
+    })
+    .catch(error => console.error(error));
+}
 
-const meuLink = document.getElementById('acervo');
-
-meuLink.addEventListener('click', function(event) {
-  // cancela o comportamento padrão da tag a
-  event.preventDefault();
-  
-  // verifica se o usuário está logado
-  if (logado) {
-    // redireciona o usuário para o arquivo2.html
-    window.location.href = 'arquivo2.html';
-  } else {
-    // redireciona o usuário para o arquivo1.html
-    window.location.href = 'arquivo1.html';
-  }
-});
-
-const meuLink2 = document.getElementById('praso');
-
-meuLink2.addEventListener('click', function(event) {
-  // cancela o comportamento padrão da tag a
-  event.preventDefault();
-  
-  // verifica se o usuário está logado
-  if (logado) {
-    // redireciona o usuário para o arquivo2.html
-    window.location.href = 'arquivo2.html';
-  } else {
-    // redireciona o usuário para o arquivo1.html
-    window.location.href = 'arquivo1.html';
-  }
-});
 
 // obtém o elemento da página que irá mostrar o nome do usuário logado
 const usuarioLogado = document.getElementById('usuarioLogado');
@@ -62,13 +37,5 @@ request.open('GET', 'obter_nome_usuario.php');
 request.send();
 
 
-// JavaScript
-// verifique se o usuário está logado e é um funcionário
-let usuarioEstaLogado = true; // exemplo de variável para usuário logado
-let usuarioEFuncionario = true; // exemplo de variável para usuário funcionário
 
-// verifique se o usuário está logado e é um funcionário
-if (usuarioEstaLogado && usuarioEFuncionario) {
-  // mostra o link para a página do funcionário
-  document.getElementById("link-funcionario").style.display = "block";
-}
+
