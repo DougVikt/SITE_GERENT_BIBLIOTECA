0a5e17d2076780359d072cade1cb6617d416c4b3
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-02-22 23:05:56 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'biblioteca';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    // Define o modo de erro do PDO para exceção
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo 'Erro ao conectar ao banco de dados: ' . $e->getMessage();
+}
+?>
