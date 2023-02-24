c3cab834d6f2c91c23b8355b795ce4b884ce7efe
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-02-24 00:21:31 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,41 +1,24 @@
+
 function verificarLogado() {
   fetch('login.php') // arquivo PHP que verifica se o usuário está logado
     .then(response => response.json())
     .then(logado => {
       if (logado) {
         // usuário está logado, então esconder botões de login e cadastro
-        document.getElementById('login').style.display = 'none';
-        document.getElementById('cadastro').style.display = 'none';
+        document.getElementById('botoes-iniciais').classList.add('d-none');
 
         // mostrar nome do usuário logado
-        document.getElementById('usuario-logado').innerHTML = nomeUsuario;
+        document.getElementById('usuarioLogado').innerHTML = nomeUsuario;
       } else {
         // usuário não está logado, então mostrar botões de login e cadastro
-        document.getElementById('login').style.display = 'block';
-        document.getElementById('cadastro').style.display = 'block';
+        document.getElementById('botoes-iniciais').classList.remove('d-none');
 
         // esconder nome do usuário logado
-        document.getElementById('usuario-logado').innerHTML = '';
+        document.getElementById('usuarioLogado').innerHTML = '';
       }
     })
     .catch(error => console.error(error));
 }
-
-
-// obtém o elemento da página que irá mostrar o nome do usuário logado
-const usuarioLogado = document.getElementById('usuarioLogado');
-
-// faz uma requisição AJAX para o servidor para obter o nome do usuário logado
-const request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-  if (this.readyState === 4 && this.status === 200) {
-    // exibe o nome do usuário logado na página
-    usuarioLogado.innerHTML = 'Bem-vindo, ' + this.responseText;
-  }
-}
-request.open('GET', 'obter_nome_usuario.php');
-request.send();
-
 
 
 
