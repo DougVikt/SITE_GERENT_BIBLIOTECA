15e8238b53db706717ceca3a3bf1c49cd1eeb66f
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-02-27 23:33:50 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,35 +1,26 @@
 
-function verificarLogado() {
-  fetch('login.php') // arquivo PHP que verifica se o usuário está logado
-    .then(response => response.json())
-    .then(logado => {
-      if (logado) {
-        // usuário está logado, então esconder botões de login e cadastro
-        document.getElementById('botoes-iniciais').classList.add('d-none');
-
-        // mostrar nome do usuário logado
-        document.getElementById('usuarioLogado').innerHTML = nomeUsuario;
-      } else {
-        // usuário não está logado, então mostrar botões de login e cadastro
-        document.getElementById('botoes-iniciais').classList.remove('d-none');
-
-        // esconder nome do usuário logado
-        document.getElementById('usuarioLogado').innerHTML = '';
-      }
-    })
-    .catch(error => console.error(error));
-}
-
-// Pega o elemento de link por id
-const link = document.getElementById('historico');
-
-// Verifica se o usuário está logado como funcionário
-if (logado && user_type === 'funcionario') {
-  link.href = 'historico.php'; // Substitui o link para funcionários
-} else if  (logado && user_type === 'usuario'){
-  link.href = 'historico_c.php'; // Substitui o link para usuários normais
-} else {
-  link.href = 'login.php';
+function verificarLogado(logado , nomeUsuario ,user_type) {
+    const link = document.getElementById('historico');
+    if (logado) {
+      // usuário está logado, então esconder botões de login e cadastro
+      document.getElementById('botoes-iniciais').classList.add('d-none');
+      document.getElementById('logout').classList.remove('d-none');
+      // mostrar nome do usuário logado
+      document.getElementById('usuarioLogado').innerHTML = nomeUsuario;
+      if (user_type === 'funcionario') {
+      link.href = 'historico_funci.php'; // Substitui o link para funcionários
+      } else if  (user_type === 'usuario'){
+      link.href = 'historico_user.php'; // Substitui o link para usuários normais
+      }  
+    } else {
+      // usuário não está logado, então mostrar botões de login e cadastro
+      document.getElementById('botoes-iniciais').classList.remove('d-none');
+      document.getElementById('logout').classList.add('d-none');
+      // esconder nome do usuário logado
+      document.getElementById('usuarioLogado').innerHTML = '';
+    }
+    
+    
 }
 
 
